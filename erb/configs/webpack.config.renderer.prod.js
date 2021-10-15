0f90d5f409e0478c53f63b17783116a2f8367314
Raine Virta <raine.virta@gmail.com>
2021-10-16 00:54:40 +0300

    fix tailwind in production build

--- erb/configs/webpack.config.renderer.prod.js
+++ erb/configs/webpack.config.renderer.prod.js
@@ -53,7 +53,16 @@ module.exports = merge(baseConfig, {
             }
           },
           'css-loader',
-          'sass-loader'
+          'sass-loader',
+
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('tailwindcss'), require('autoprefixer')]
+              }
+            }
+          }
         ]
       },
       // WOFF Font
