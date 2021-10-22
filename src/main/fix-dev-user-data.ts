0f5b4f0338e8d0875f063eea0e8b20f5e92cf06b
Raine Virta <raine.virta@gmail.com>
2021-10-22 20:54:32 +0300

    move electron-builder config to electron-builder.json

--- src/main/fix-dev-user-data.ts
+++ src/main/fix-dev-user-data.ts
@@ -3,7 +3,7 @@ import * as path from 'path'
 
 // https://github.com/electron-react-boilerplate/electron-react-boilerplate/issues/2197
 if (process.env.NODE_ENV === 'development') {
-  const { build } = require('../../package.json')
+  const pkg = require('../../package.json')
   const appData = app.getPath('appData')
-  app.setPath('userData', path.join(appData, build.productName))
+  app.setPath('userData', path.join(appData, pkg.productName))
 }
