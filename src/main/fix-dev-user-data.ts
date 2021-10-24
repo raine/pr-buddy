5c5efb1a944800db4a0230696d249f659f67060f
Raine Virta <raine.virta@gmail.com>
2021-10-24 23:25:30 +0300

    set different userdata dir for dev

--- src/main/fix-dev-user-data.ts
+++ src/main/fix-dev-user-data.ts
@@ -5,5 +5,5 @@ import * as path from 'path'
 if (process.env.NODE_ENV === 'development') {
   const pkg = require('../../package.json')
   const appData = app.getPath('appData')
-  app.setPath('userData', path.join(appData, pkg.productName))
+  app.setPath('userData', path.join(appData, pkg.productName + ' dev'))
 }
