6ef2c429deb8cb03f467eb8875098d3814fa9a30
Raine Virta <raine.virta@gmail.com>
2021-10-28 00:24:49 +0300

    change error notification style

--- src/renderer/PullRequestList.tsx
+++ src/renderer/PullRequestList.tsx
@@ -67,8 +67,8 @@ export default function PullRequestList({
     return (
       <div className="p-5">
         {error === 'GENERIC_HTTP_ERROR' && (
-          <div className="text-sm bg-red-500 text-gray-100 rounded px-4 py-2 mb-2 text-shadow-sm">
-            ERROR: Could not fetch pull requests from GitHub
+          <div className="font-medium bg-red-100 text-red-600 rounded px-4 py-2 mb-2 text-shadow-sm-fff">
+            Error: Could not fetch pull requests from GitHub
           </div>
         )}
         {data?.result === 'OK' &&
